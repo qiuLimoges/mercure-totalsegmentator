7eb4cefbf4ae8b3f31ccbd249dfdfe6848229c14
qiuLimoges <qiuhao2007@gmail.com>
2024-07-17 22:09:10 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
 echo "-- Starting TotalSegmentator..."
-conda run -n mercure-totalsegmentator python mercure-totalsegmentator -i $MERCURE_IN_DIR -o $MERCURE_OUT_DIR
+conda run -n mercure-totalsegmentator python mercure-totalsegmentator -i $MERCURE_IN_DIR -o $MERCURE_OUT_DIR --output_type dicom
 echo "-- Done."
